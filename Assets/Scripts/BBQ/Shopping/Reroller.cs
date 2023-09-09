75b3e47d9c4b3838cf6b98297b626635fc530461
asada0815 <asd0815electric@gmail.com>
2023-09-09 18:56:07 +0900

    fix

---
+++ Assets/Scripts/BBQ/Shopping/Reroller.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BBQ.Common;
+using BBQ.Database;
+using Cysharp.Threading.Tasks;
+using SoundMgr;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BBQ.Shopping {
+    public class Reroller : MonoBehaviour {
+
+        private Shop _shop;
+        private Coin _coin;
+        [SerializeField] private ShopItemChoice choice;
+
+        private bool _isWaiting;
+
+        private int _cost;
+
+        public void Init(Shop shop, Coin coin) {
+            _shop = shop;
+            _coin = coin;
+            _cost = 5;
+            _isWaiting = false;
+            transform.Find("Cost").GetComponent<Text>().text = "-" + _cost;
+        }
+        
+        public async void Reroll() {
+            _isWaiting = true;
+            List<ShopItem> shopItems = _shop.GetShopItems();
+            if(shopItems != null) _shop.DeleteItems(new List<ShopItem>(shopItems));
+            List<FoodData> foods = choice.Choice(1);
+            await _shop.AddItems(foods);
+            _isWaiting = false;
+        }
+        
+        public void OnClickRerollButton() {
+            if(_isWaiting) return;
+            if (_coin.GetCoin() < _cost) return;
+            SoundPlayer.I.Play("se_reroll1");
+            SoundPlayer.I.Play("se_reroll2");
+            _coin.Use(_cost);
+            _cost += 5;
+            transform.Find("Cost").GetComponent<Text>().text = "-" + _cost;
+            Reroll();
+        }
+
+
+
+    }
+}
